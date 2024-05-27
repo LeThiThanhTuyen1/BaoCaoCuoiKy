48674f1c80620309163131b7c8a1d3b466f5b08a
LeThiThanhTuyen1 <thanhtuyen162003@gmail.com>
2024-05-27 11:24:03 +0700

    layout login

--- InventoryManagement/InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c5e7bef9ad02e73e83e03b1a62d1adf80d0693a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c65f46396a3517b74ff8c5ef1c8683399932ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
