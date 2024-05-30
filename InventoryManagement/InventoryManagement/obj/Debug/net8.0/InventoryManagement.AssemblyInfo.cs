ef3f4e0fdd68d011be397b26bbcfe269b9f7f0b4
LeThiThanhTuyen1 <thanhtuyen162003@gmail.com>
2024-05-30 13:25:08 +0700

    fix _layout

--- InventoryManagement/InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
+++ InventoryManagement/InventoryManagement/obj/Debug/net8.0/InventoryManagement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+870d000dcb08981b1d14d84bc15d672148f0efb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178331a8bc07217ba900645fc2d6f77b5bd621cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
